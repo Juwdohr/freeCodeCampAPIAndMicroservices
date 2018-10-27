90f13150de3112709b57cf67d2225eaed32597f6
Glitch (stitch-supermarket) <none>
2018-10-27 17:13:48 +0000

    Added API for whoami project

--- server.js
+++ server.js
@@ -14,20 +14,25 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
+app.get("/timestamp", (req, res) => res.sendFile(__dirname + '/views/timestamp/index.html'));
+
+app.get("/whoami", (req, res) => res.sendFile(__dirname + '/views/whoami/index.html'));
 
 // your first API endpoint... 
-app.get("/api/timestamp/:date_string?", function (req, res) {
+app.get("/api/timestamp/:date_string?", (req, res) => {
   const date = req.date_string===undefined ? new Date() : new Date(req.date_string);
   res.json({"unix": date.getTime(), "utc": date.toUTCString()})
 });
 
+app.get("/api/whoami", (req, res) => {
+  const {headers} = req;
+  res.json({"ipaddress": headers['x-forwarded-for'].split(',')[0], "language": headers['accept-language'], "software": headers['user-agent']})
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
